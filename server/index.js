9026f4cd857719f55b683d7f12dc1b0b4f86dc7b
Riddhi2409 <98161523+Riddhi2409@users.noreply.github.com>
2023-06-16 17:40:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const authRoutes=require('./router/UserAuth')
 const TaskRoutes=require('./router/TaskForm')
 
+const port=process.env.PORT || 8080;
+
 const connectdb=require('./mongodb/connect');
 const app=express();
 app.use(express.json({ limit: '50mb' }))
@@ -14,7 +16,7 @@ app.use('/task',TaskRoutes)
 const startServer = async () => {
     try {
       connectdb.connectdb(process.env.MONGO_URL);
-      app.listen(8080, () => console.log('Server started on port 8080'));
+      app.listen(port, () => console.log('Server started on port 8080'));
     } catch (error) {
       console.log(error);
     }
